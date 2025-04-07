cc188424b00b8f16ecf39e48e7504d346ccd3d90
Himanshu-n17 <nayak.himanshu2004@gmail.com>
2025-04-07 21:31:00 +0530

    PORTFOLIO

--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,3 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-
-const rootElement = document.getElementById("root");
-if (!rootElement) {
-  throw new Error("Failed to find the root element");
-}
-
-createRoot(rootElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
